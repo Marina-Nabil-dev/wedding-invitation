804b768262b66fab1dd0d9521ad44967ad06e2d9
MarinaNabil <marina.nabil@biddex.com>
2026-09-17 22:32:40 +0300

    my wedding invitation 3

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.INVITATION_CONFIG = {
-  appsScriptUrl: '',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbyhXUhScg7WGSjtLx_4U8RcrtYmBiF9rpAKKAix95-DkR-_8CDlMRGjRGwNnfOQVETT/exec',
   eventDate: '2026-10-10T22:00:00+03:00',
   rsvpDeadline: '2026-10-01T23:59:59+03:00',
   capacity: 70,
